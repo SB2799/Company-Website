c94a6fca473ad90e7bda6ec8956269bf7681ddf7
Sourav Biswas <sourav.biswas2799@gmail.com>
2023-06-04 17:26:29 +0530

    React Ref

--- src/ContextImage.jsx
+++ src/ContextImage.jsx
@@ -7,13 +7,18 @@ const ContextImage = () => {
 
   const[name, setName] = useState("");
   const refCount  = useRef(0)
+  const previousCount  = useRef("")
+  const refDOM  = useRef();
 
   useEffect(()=>{
     refCount.current = refCount.current + 1;
     previousCount.current = name;
   },[name])
 
-  const previousCount  = useRef("")
+  const refClick = () => (
+    refDOM.current.style.border = "1px solid red"
+  )
+
 
   const changeInfo = (e) => {
     setName(e.target.value);
@@ -40,12 +45,13 @@ const ContextImage = () => {
         <h1>Use Ref Uses</h1>
           <div className = "col xl-3 m-3 p-3">
         <br />
-      <input type = "text" placeholder value = {name} onChange = {(e) => changeInfo(e)}/>
+      <input ref={refDOM} type = "text" placeholder="enter text" value = {name} onChange = {(e) => changeInfo(e)}/>
       <br />
       <h2>{refCount.current}</h2>
       <br />
       <h2>The previous ref state is: {previousCount.current}</h2>
       <br />
+      <button className="btn btn-secondary" onClick = {refClick}>Use ref</button>
         </div>
         </div>
       </div>
